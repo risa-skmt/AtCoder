747d5eafdd44ef329aa5c390ca3d3f3f76918356
risa-skmt <skmt.risa@gmail.com>
2023-07-08 19:32:46 +0900

    sort途中

--- contest_abc_308.js
+++ contest_abc_308.js
@@ -31,3 +31,41 @@ reader.on("close", () => {
   //console.log(result);
   console.log(result.join(" "));
 });
+
+
+
+
+
+
+//sortを1巡しかできていない。
+//長さ-1回同じ処理を繰り返す
+function Main(input){
+  input = input.split("\n");
+ 
+   
+  for(let i=1; i<=input[0]-1; i++){
+    console.log(i);
+    console.log(input);
+    let A = input[i];
+    let B = input[i+1];
+    console.log(A);
+    console.log(B);
+    let a = A.split(" ");
+    let b = B.split(" ");
+    //console.log(b);
+    let aProb = Number(a[0]) * (Number(b[0])+ Number(b[1]));
+    let bProb = Number(b[0]) * (Number(a[0])+ Number(a[1]));
+    console.log(aProb);
+     console.log(bProb);
+    
+    if(aProb < bProb){
+      console.log("-----------")
+      input[i] = B;
+      input[i+1] = A;
+      console.log(input);
+      console.log("-----------")
+    }
+  }
+ 
+ }
+ Main(require("fs").readFileSync("/dev/stdin", "utf8"));
