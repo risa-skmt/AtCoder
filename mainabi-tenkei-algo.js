333fb1a77d8eade7b5f5edfe5f013d679890eba4
risa-skmt <skmt.risa@gmail.com>
2023-07-10 15:33:45 +0900

    second commit

--- mainabi-tenkei-algo.js
+++ mainabi-tenkei-algo.js
@@ -27,4 +27,37 @@ function Main(input){
     }
     
   }
-  Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+
+//まだエラーあり。理由わからん。
+//   Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
+
+//   function Main(input){
+//     input = input.trim().split("\n");
+//     let line1= input[0].split(" ");
+//     let length = Number(line1[0]);
+//     let border = Number(line1[1]);
+//     let array = input[1].split(" ");
+    
+    
+//     if(array[length-1] < border){
+//      console.log(-1);
+//     }
+//     if(array[0] >= border){
+//      console.log(0);
+//     }
+    
+//     let left = 0;
+//     let right = length-1;
+//     while((right - left) > 1){
+//       let center = Math.ceil((right+left)/2);
+//       if(array[center] >= border){
+//           right = center;
+//       }else {
+//           left = center
+//       }
+//     }
+//     console.log(right);
+//   }
+//   Main(require("fs").readFileSync("/dev/stdin", "utf8"));
